d9d3871736a4a995360b98438f01b1896e9b4698
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2025-03-11 14:41:48 +0500

    Add loading spinner component with react-loader-spinner
    - Install react-loader-spinner package
    - Create Spinner component with Vortex loader
    - Add Spinner to App.jsx
    - Configure spinner with custom styling and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Header from "./components/Header"
+import Spinner from "./components/Spinner"
 
 function App() {
   return (
     <>
       <Header />
+      <Spinner />
     </>
   )
 }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,20 @@
+import { Vortex } from "react-loader-spinner";
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-500 bg-opacity-50">
+      <Vortex
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{}}
+        wrapperClass="vortex-wrapper"
+        colors={["red", "green", "blue", "yellow", "orange", "purple"]}
+      />
+    </div>
+  );
+};
+
+export default Spinner;
