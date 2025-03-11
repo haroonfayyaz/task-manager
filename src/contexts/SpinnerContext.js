5276f3bcb9629a1bf871794e7d14ddb1c68b04b0
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2025-03-11 15:34:55 +0500

    code formatted

--- src/contexts/SpinnerContext.js
+++ src/contexts/SpinnerContext.js
@@ -1,5 +1,5 @@
-import React from "react";
+import React from "react"
 
-const SpinnerContext = React.createContext();
+const SpinnerContext = React.createContext()
 
-export default SpinnerContext;
+export default SpinnerContext
